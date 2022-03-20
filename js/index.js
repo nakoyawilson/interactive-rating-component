b56f6e065bc2d53f1eadc995c4963fb409942b17
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-03-20 10:40:30 -0400

    Improve functionality and delete unncessary code

--- js/index.js
+++ js/index.js
@@ -1,22 +1,13 @@
 const ratingsForm = document.getElementById("ratings-form");
-const inputs = document.querySelectorAll(".ratings-input");
 const ratings = document.getElementById("ratings");
 const thanks = document.getElementById("thanks");
 
-inputs.forEach((input) => {
-  input.addEventListener("change", () => {
-    if (input.checked) {
-      input.nextElementSibling.classList.add("active");
-    }
-  });
-});
-
 ratingsForm.addEventListener("submit", (e) => {
-  const userRating = document.querySelector(
-    'input[name="rating"]:checked'
-  ).value;
-  ratings.classList.toggle("hide-section");
-  thanks.classList.toggle("hide-section");
-  document.getElementById("user-choice").innerHTML = userRating;
   e.preventDefault();
+  const userRating = document.querySelector('input[name="rating"]:checked');
+  if (userRating !== null) {
+    ratings.classList.toggle("hide-section");
+    thanks.classList.toggle("hide-section");
+    document.getElementById("user-choice").innerHTML = userRating.value;
+  }
 });
